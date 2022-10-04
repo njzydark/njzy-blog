ebe61ee6fced58c0c21b7dd9d33084c4fb7b8fcf
njzy <njzydark@gmail.com>
2022-10-04 14:37:12 +0800

    refactor: DarkModeToggle

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,100 +1,24 @@
 import cs from 'classnames';
 import { useTheme } from 'next-themes';
-import { CSSProperties, MouseEventHandler, useEffect, useState } from 'react';
-import { Monitor, Moon, Sun } from 'react-feather';
-
-const themes = ['light', 'dark', 'system'];
+import { useEffect } from 'react';
+import { Moon, Sun } from 'react-feather';
 
 export const DarkModeToggle = () => {
-  const { theme, setTheme, resolvedTheme } = useTheme();
-
-  const isSystemTheme = theme === 'system';
-
-  const curTheme = isSystemTheme ? 'system' : resolvedTheme === 'dark' ? 'dark' : 'light';
-
-  const [hasMounted, setHasMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
 
   useEffect(() => {
-    setHasMounted(true);
-  }, []);
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      setTheme('system');
+    });
+  }, [setTheme]);
 
-  const [dropMenuStyle, setDropMenuStyle] = useState<CSSProperties>({
-    position: 'absolute',
-    top: -999,
-    opacity: 0,
-    width: 80,
-  });
-
-  useEffect(() => {
-    if (dropMenuStyle.opacity === 0) {
-      return;
-    }
-
-    const handleClose = (event: MouseEvent) => {
-      const target = event.target as HTMLElement;
-      if (target.closest('#darkModeToggleWrapper') === null) {
-        setDropMenuStyle(pre => ({
-          ...pre,
-          top: -999,
-          opacity: 0,
-        }));
-      }
-    };
-
-    document.addEventListener('click', handleClose);
-
-    return () => {
-      document.removeEventListener('click', handleClose);
-    };
-  }, [dropMenuStyle.opacity]);
-
-  const handleClick: MouseEventHandler<HTMLDivElement> = event => {
-    const target = event.currentTarget as HTMLDivElement;
-    const { bottom, right } = target.getBoundingClientRect();
-    setDropMenuStyle(pre => ({
-      ...pre,
-      opacity: 1,
-      left: right - (dropMenuStyle.width as number),
-      top: bottom + 10,
-    }));
+  const handleToggle = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
   };
 
-  if (!hasMounted) {
-    return null;
-  }
-
   return (
-    <div id="darkModeToggleWrapper">
-      <div
-        className={cs(
-          'hover:bg-gray-100 p-2 hover:dark:text-white box-border hover:dark:bg-gray-600',
-          dropMenuStyle.opacity === 1 && 'bg-gray-100 dark:bg-gray-600'
-        )}
-        onClick={handleClick}
-      >
-        {isSystemTheme ? <Monitor /> : curTheme === 'dark' ? <Moon /> : <Sun />}
-      </div>
-      <div className="drop-shadow-lg bg-white dark:bg-gray-600 z-10 transition-opacity" style={dropMenuStyle}>
-        {themes.map(theme => (
-          <div
-            key={theme}
-            className={cs(
-              'px-3 py-1 hover:bg-blue-500 hover:text-white cursor-pointer',
-              theme === curTheme && ' text-blue-500'
-            )}
-            onClick={() => {
-              setTheme(theme);
-              setDropMenuStyle(pre => ({
-                ...pre,
-                opacity: 0,
-                top: -999,
-              }));
-            }}
-          >
-            {theme[0].toUpperCase() + theme.slice(1)}
-          </div>
-        ))}
-      </div>
+    <div className={cs('px-2 hover:cursor-pointer hover:dark:text-white')} onClick={handleToggle}>
+      {resolvedTheme === 'dark' ? <Moon /> : <Sun />}
     </div>
   );
 };
